95d1a40977344faf84c2da1c7b7919b404df22ef
Muhammad Ahsan Khan <97152316+ahsann455@users.noreply.github.com>
2023-07-21 12:05:25 +0500

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Passionate Software Engineering Student with a Focus on Machine Learning & AI.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Python",
+  "C++",
+  "TensorFlow",
+  "Deeplearning",
+  "Machine learning Algorithms",
+  "Data Analysis",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Driven by a desire to deepen my understanding and keep up with the latest advances in the field, I am actively self-taught, constantly exploring new frameworks, libraries, and research papers. I am particularly excited about using AI to improve user experience, optimize decision-making processes, and revolutionize industries through innovative solutions.";
 
 const About = () => {
   return (
